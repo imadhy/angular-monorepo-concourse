8bed9ebce9abdc18b678e91c896b5f5a9023949f
Imad El Hitti <imadhy.gt@gmail.com>
2019-10-17 15:26:03 +0200

    ci(concourse): fix ng analytics

--- ci/test.sh
+++ ci/test.sh
@@ -2,6 +2,7 @@
 
 echo "Node Version: $(node --version)"
 echo "NPM Version: $(npm --version)"
+export NG_CLI_ANALYTICS=ci
 cd angular-monorepo-concourse
 npm install
 npm run affected:test -- --parallel

c67d884a30c4be44610279cd4009a823c867bb28
Imad El Hitti <imadhy.gt@gmail.com>
2019-10-18 16:48:09 +0200

    ci(concourse): add npm install cmd for build tasks

--- ci/build_pn106.sh
+++ ci/build_pn106.sh
@@ -2,5 +2,6 @@
 echo "Node Version: $(node --version)"
 echo "NPM Version: $(npm --version)"
 cd angular-monorepo-concourse
+npm install
 npm run build -- --project=pn106
 cp -R dist ../livrable_pn106/
--- ci/build_pn107.sh
+++ ci/build_pn107.sh
@@ -2,5 +2,6 @@
 echo "Node Version: $(node --version)"
 echo "NPM Version: $(npm --version)"
 cd angular-monorepo-concourse
+npm install
 npm run build -- --project=pn107
 cp -R dist ../livrable_pn107/
